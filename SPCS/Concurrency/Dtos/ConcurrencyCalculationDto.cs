48bc7417615162ed5247bc2801d839dcb8c945bb
Chris Milios <cmilios@hotmail.com>
2025-07-11 01:54:54 +0300

    Better provide calculation data to front

--- SPCS/Concurrency/Dtos/ConcurrencyCalculationDto.cs
+++ SPCS/Concurrency/Dtos/ConcurrencyCalculationDto.cs
@@ -3,7 +3,9 @@
     public class ConcurrencyCalculationDto
     {
         public int Id { get; init; }
-        public List<PowerTimestampDto> PowerTimestamps { get; set; } = new();
+        public List<PowerTimestampDto> BatteryHistory { get; set; } = default!;
+        public List<PowerTimestampDto> PowerToTheNetwork { get; set; } = default!;
+        public List<PowerTimestampDto> PowerFromTheNetwork { get; set; } = default!;
         public decimal ConcurrencyMetric { get; set; }
     }
 }
--- SPCS/Concurrency/Mappers/ConcurrencyCalculationDtoMapper.cs
+++ SPCS/Concurrency/Mappers/ConcurrencyCalculationDtoMapper.cs
@@ -1,4 +1,5 @@
 ﻿using SPCS.Concurrency.Dtos;
+using SPCS.Concurrency.Enum;
 using SPCS.Concurrency.Models;
 
 namespace SPCS.Concurrency.Mappers
@@ -9,7 +10,9 @@ namespace SPCS.Concurrency.Mappers
         {
             return new ConcurrencyCalculationDto
             {
-                PowerTimestamps = [.. source.PowerTimestamps.Select(x => PowerTimestampDtoMapper.Map(x))],
+                BatteryHistory = [.. source.PowerTimestamps.Where(x => x.Type == PowerTimestampType.BatteryHistory).Select(x => PowerTimestampDtoMapper.Map(x))],
+                PowerFromTheNetwork = [.. source.PowerTimestamps.Where(x => x.Type == PowerTimestampType.PowerFromTheNetwork).Select(x => PowerTimestampDtoMapper.Map(x))],
+                PowerToTheNetwork = [.. source.PowerTimestamps.Where(x => x.Type == PowerTimestampType.PowerToTheNetwork).Select(x => PowerTimestampDtoMapper.Map(x))],
                 ConcurrencyMetric = source.ConcurrencyMetric,
                 Id = source.Id,
 
